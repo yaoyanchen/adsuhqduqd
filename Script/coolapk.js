7616a963b9240d6f04dabf0c25a8e5f8c4f268f2
yaoyanchen <61932075+yaoyanchen@users.noreply.github.com>
2022-11-18 13:45:38 +0800

    Update coolapk.js

--- Script/coolapk.js
+++ Script/coolapk.js
@@ -7,7 +7,7 @@ author      kk pp
 
 
 [rewrite_local]
-^https?:\/\/api.coolapk.com\/v6\/(feed\/(replyList|detail)|main\/indexV8|dataList) url script-response-body https://ocd0522.tk/ddgksf2013/Cuttlefish/raw/branch/master/Script/coolapk.js
+^https?:\/\/api.coolapk.com\/v6\/(feed\/(replyList|detail)|main\/indexV8|dataList) url script-response-body https://raw.githubusercontent.com/yaoyanchen/adsuhqduqd/master/Script/coolapk.js
 
 [mitm]
 hostname = api.coolapk.com
